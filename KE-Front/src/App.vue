63b3544ca57323814117949eaa277975727e9c04
huntermali <hemalino@mtu.edu>
2022-11-03 21:25:48 -0400

    Update App.vue

--- KE-Front/src/App.vue
+++ KE-Front/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Keweenaw Exploration"/>
+    <h2>Welcome to</h2>
+    <h1>Keweenaw Exploration</h1>
+    <img src="C:\Users\hunte\OneDrive\Documents\GitHub\CS3141-R02-team13\KE-Front\src\assets\Keweenaw.jpg" alt="Picture of the Keweenaw"><br>
+    <button id = "loc">Explore Locations</button>
   </div>
 </template>
 
+<!--
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -14,6 +18,10 @@ export default {
   }
 }
 </script>
+-->
+
+<link rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Kalam">
 
 <style>
 #app {
@@ -23,5 +31,40 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#loc {
+  font-family: 'Kalam';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 48px;
+  line-height: 77px;
+  text-align: center;
+  color: #6A8D65;
+  box-sizing: border-box;
+  background: #FFFFFF;
+  border-radius: 30px;
+  border: none;
+}
+body {
+  background: #6A8D65;
+}
+h2 {
+  font-family: 'Kalam';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 48px;
+  line-height: 77px;
+  text-align: center;
+  color: #FFFFFF;
+}
+h1 {
+  font-family: 'Kalam';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 64px;
+  line-height: 102px;
+  text-align: center;
+
+  color: #FFFFFF;
 }
 </style>
