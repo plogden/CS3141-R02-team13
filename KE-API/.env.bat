1c094669c8c85b8a2d86580a9f8b40d562bf9452
Andrew <andrew.koman.02@gmail.com>
2022-11-03 21:29:10 -0400

    Fixed Database connection and installed mysql driver

--- KE-API/.env.bat
+++ KE-API/.env.bat
@@ -5,5 +5,5 @@ set DB_NAME=keexplore
 set DB_HOST=classdb.it.mtu.edu
 set DB_PORT=3307
 set DB_USER=keexplore_rw
-set DB_PASSWORD="RE?F9&,h&kaaiE9z2KFl"
+set DB_PASSWORD=RE?F9&,h&kaaiE9z2KFl
 set DB_DIALECT=mysql
