850e28d87cb9c9cf67c26065f43052add0c3f387
= <=>
2022-11-15 17:30:25 -0500

    Setup multipage with empty locations page

--- KE-Front/src/main.js
+++ KE-Front/src/main.js
@@ -1,4 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
 
-createApp(App).mount('#app')
+import App from "@/App.vue";
+import HomePage from "@/views/HomePage.vue";
+import LocationsList from "@/views/LocationsList.vue";
+
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomePage,
+  },
+  {
+    path: "/locations",
+    name: "LocationsList",
+    component: LocationsList,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+createApp(App).use(router).mount("#app");
---
+++ KE-Front/src/views/LocationsList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+    <h1>Locations</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LocationsList',
+
+}
+</script>
+
+<style>
+@font-face {
+  font-family: "Kalam";
+  src: local("Kalam"),
+  url(@/assets/Kalam-Regular.ttf) format("truetype");
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+#loc {
+  font-family: 'Kalam';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 30px;
+  text-align: center;
+  color: #6A8D65;
+  box-sizing: border-box;
+  background: #FFFFFF;
+  border-radius: 30px;
+  border: none;
+  padding: 5px 15px 5px 15px;
+}
+body {
+  background: #6A8D65;
+  margin: 0;
+  padding: 0;
+}
+h2 {
+  font-family: 'Kalam';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 30px;
+  text-align: center;
+  color: #FFFFFF;
+  margin-bottom: 0;
+  margin-top: 5%;
+}
+h1 {
+  font-family: 'Kalam';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 48px;
+  text-align: center;
+  margin: 0;
+  color: #FFFFFF;
+}
+img {
+  width: 30%
+}
+</style>
